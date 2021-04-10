dd75cf2cbad68165862b59d49079152686848cfa
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-10 21:01:02 +0200

    Adds function def.

--- countAces.hs
+++ countAces.hs
@@ -1,3 +1,6 @@
 data Suit = Hearts | Clubs | Diamonds | Spades deriving Eq
 data Rank = Numeric Int | Jack | Queen | King | Ace deriving Eq
 data Card = NormalCard Rank Suit | Joker deriving Eq
+
+countAces :: [Card] -> Int
+
