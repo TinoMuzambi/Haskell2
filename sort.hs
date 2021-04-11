990e5023231895a0388fe999f25a9a6725cbf2cd
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-11 17:11:48 +0200

    Adds helper sort function.

--- sort.hs
+++ sort.hs
@@ -1,2 +1,8 @@
 sort :: [Int] -> [Int]
 
+mySort []  = []
+mySort [x] = [x]
+mySort (x:xs) | x > head xs = head xs:mySort (x:tail xs)
+            | otherwise = x:mySort xs 
+
+sort xs = foldl (\acc e -> mySort acc) xs xs
