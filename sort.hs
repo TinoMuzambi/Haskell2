a5290577343ea66fd8b4ca6bdccb09f81324e315
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-11 17:16:53 +0200

    Adds head logic.

--- sort.hs
+++ sort.hs
@@ -1,7 +1,10 @@
+mySort :: (Ord a) => [a] -> [a]
+
 mySort []  = []
 mySort [x] = [x]
 mySort (x:xs) | x > head xs = head xs:mySort (x:tail xs)
             | otherwise = x:mySort xs 
 
 sort :: [Int] -> [Int]
+
 sort xs = foldl (\acc e -> mySort acc) xs xs
