8df416a9a9b362bdb2fbbcf47f0f7fb140681aa5
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-11 17:15:09 +0200

    Adds base cases.

--- sort.hs
+++ sort.hs
@@ -1,8 +1,7 @@
-sort :: [Int] -> [Int]
-
 mySort []  = []
 mySort [x] = [x]
 mySort (x:xs) | x > head xs = head xs:mySort (x:tail xs)
             | otherwise = x:mySort xs 
 
+sort :: [Int] -> [Int]
 sort xs = foldl (\acc e -> mySort acc) xs xs
