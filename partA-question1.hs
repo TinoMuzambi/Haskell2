3b34f53bb41a7df70eba8ed197d7077343541598
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-12 16:40:54 +0200

    Adds bf and df functions.

--- partA-question1.hs
+++ partA-question1.hs
@@ -1,1 +1,9 @@
-gf :: (a -> a) -> [a] -> [a]
+-- gf :: (a -> a) -> [a] -> [a]
+
+bf,df :: [Int] -> [Int]
+bf [] = []
+bf [x] = [abs x] 
+bf (x:y:xs) = (abs x) : y: bf xs 
+df [] = []
+df [x] = [x + 1] 
+df (x:y:xs) = (x+1) : y : df xs
