a86ed2efd8005c2de8ac8e4573ecf5b01058617a
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-29 22:27:45 +0200

    Adds show and format for counting.

--- partA-question2.hs
+++ partA-question2.hs
@@ -1,10 +1,10 @@
-data Suit = Hearts | Clubs | Diamonds | Spades deriving Eq
-data Rank = Numeric Int | Jack | Queen | King | Ace deriving Eq
-data Card = NormalCard Rank Suit | Joker deriving Eq
+data Suit = Hearts | Clubs | Diamonds | Spades deriving (Eq, Show)
+data Rank = Numeric Int | Jack | Queen | King | Ace deriving (Eq, Show)
+data Card = NormalCard Rank Suit | Joker deriving (Eq, Show)
 
 countAces :: [Card] -> Int
 
 countAces [] = 0
-countAces (n:ns) = if n == Joker || n == NormalCard Ace _
+countAces (n:ns) = if n == Joker || n == NormalCard Ace Hearts || n == NormalCard Ace Clubs || n == NormalCard Ace Diamonds || n == NormalCard Ace Spades
                         then 1 + countAces (tail ns)
                         else 0 + countAces (tail ns)
